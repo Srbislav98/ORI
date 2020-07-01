f8d5449a9063ccfb13e7ce31f130a74e1e8500a0
JovanCorilic <kssbc39@gmail.com>
2020-07-01 15:14:34 +0200

    Postavljanje lista

---
+++ Predefinisani projekat 2 Prvi projekat/Kartica.py
@@ -0,0 +1,20 @@
+class Kreditna_Kartica:
+    def __init__(self, q,w,e,r,t,y,u,m,o,p,a,s,d,f,g,h,j,k):
+        self.CUSTID = q
+        self.BALANCE = w
+        self.BALANCEFREQUENCY = e
+        self.PURCHASES =r
+        self.ONEOFFPURCHASES=t
+        self.INSTALLMENTSPURCHASES=y
+        self.CASHADVANCE =u
+        self.PURCHASESFREQUENCY =m
+        self.ONEOFFPURCHASESFREQUENCY =o
+        self.PURCHASESINSTALLMENTSFREQUENCY =p
+        self.CASHADVANCEFREQUENCY=a
+        self.CASHADVANCETRX=s
+        self.PURCHASESTRX =d
+        self.CREDITLIMIT =f
+        self.PAYMENTS=g
+        self.MINIMUM_PAYMENTS=h
+        self.PRCFULLPAYMENT=j
+        self.TENURE=k
--- Predefinisani projekat 2 Prvi projekat/Main.py
+++ Predefinisani projekat 2 Prvi projekat/Main.py
@@ -1,15 +1,67 @@
 import numpy as np
 import csv
-
+from Kartica import Kreditna_Kartica
 
 
 if __name__ == '__main__':
     kratko = open('credit_card_data.csv', encoding='utf-8')
     reader = csv.reader(kratko)
+    listaCUSTID = []
     listaBALANCE = []
-    
+    listaBALANCEFREQUENCY   = []
+    listaPURCHASES  = []
+    listaONEOFFPURCHASES  = []
+    listaINSTALLMENTSPURCHASES  = []
+    listaCASHADVANCE  = []
+    listaPURCHASESFREQUENCY  = []
+    listaONEOFFPURCHASESFREQUENCY  = []
+    listaPURCHASESINSTALLMENTSFREQUENCY  = []
+    listaCASHADVANCEFREQUENCY  = []
+    listaCASHADVANCETRX  = []
+    listaPURCHASESTRX  = []
+    listaCREDITLIMIT  = []
+    listaPAYMENTS  = []
+    listaMINIMUM_PAYMENTS  = []
+    listaPRCFULLPAYMENT  = []
+    listaTENURE  = []
+    listaSvega = []
+
     for row in reader:
+        if row[0] == "CUST_ID":
+            continue
+        else:
+            listaCUSTID.append(row[0])
+            listaBALANCE.append(float(row[1]))
+            listaBALANCEFREQUENCY.append(float(row[2]))
+            listaPURCHASES.append(float(row[3]))
+            listaONEOFFPURCHASES.append(float(row[4]))
+            listaINSTALLMENTSPURCHASES.append(float(row[5]))
+            listaCASHADVANCE.append(float(row[6]))
+            listaPURCHASESFREQUENCY.append(float(row[7]))
+            listaONEOFFPURCHASESFREQUENCY.append(float(row[8]))
+            listaPURCHASESINSTALLMENTSFREQUENCY.append(float(row[9]))
+            listaCASHADVANCEFREQUENCY.append(float(row[10]))
+            listaCASHADVANCETRX.append(float(row[11]))
+            listaPURCHASESTRX.append(float(row[12]))
+            if row[13] == "":
+                row[13] = 0
+            listaCREDITLIMIT.append(float(row[13]))
+            listaPAYMENTS.append(float(row[14]))
+
+            if row[15]=="":
+                row[15]=0
+                listaMINIMUM_PAYMENTS.append(0)
+            else:
+                listaMINIMUM_PAYMENTS.append(float(row[15]))
+
+            listaPRCFULLPAYMENT.append(float(row[16]))
+            listaTENURE.append(float(row[17]))
+            listaSvega.append(Kreditna_Kartica(row[0],float(row[1]),float(row[2]),float(row[3]),float(row[4]),
+                                               float(row[5]),float(row[6]),float(row[7]),float(row[8]),float(row[9]),
+                                               float(row[10]),float(row[11]),float(row[12]),float(row[13]),float(row[14]),float(row[15]),
+                                               float(row[16]),float(row[17])))
 
 
-    max_value = np.max(arr)
+    
+    max_value = np.max(listaBALANCE)
     print('Maximum value of the array is',max_value)
